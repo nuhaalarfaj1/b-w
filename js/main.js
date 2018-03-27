cdc86789cf1229f412f9d1a5012ca712b2a08cde
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-03-26 17:13:30 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+// var colors =[
+//     {name:"black",
+//     img:,
+//  say:"People who choose black as their favorite color are often artistic and sensitive."
+//     },
+//     {name:"white",
+//     img:,
+// say: People who like white are often organized and logical and don't
+//  have a great deal of clutter in their lives.
+//     }
+// ];
+
+var colors = [
+   { name: 1,
+   means: ' you are often artistic and sensitive'
+},
+{
+    name:2,
+    means:'you are often organized and logical',
+    
+}
+
+];
+var input = document.getElementById("colorsname");
+var myh1 = document.getElementById("name");
+var myh2 = document.getElementById("means");
+
+
+
+function getinfo(){
+    for (var i=0 ; i < colors.length; i++){
+        if(input.value == colors[i].name){
+myh1.innerText = colors[i].name;
+myh2.innerText = colors[i].means;
+        }
+    };
+
+}
+document.body.style.color = "yellow";
+document.body.style.fontSize= "100px";
