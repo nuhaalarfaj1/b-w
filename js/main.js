751a4adcd0896b00da2d19ad1e3ce47862c19aab
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-03-30 15:39:49 -0700

    first

--- js/main.js
+++ js/main.js
@@ -12,37 +12,37 @@
 // ];
 
 var colors = [
-   { name: "black",
-   means: ' you are often artistic and sensitive',
+   { name: "Black",
+   means: ' You are often artistic and sensitive',
    objImage: 'img/1.jpg'
 
 },
 {
-    name:"white",
-    means:'you are often organized and logical',
+    name:"White",
+    means:'You are often organized and logical',
     objImage:'img/2.jpg'
 },
 {
-    name:"red",
-    means:'you are action oriented with a deep need for physical fulfillment and to experience life through the five senses.',
+    name:"Red",
+    means:'You are action oriented with a deep need for physical fulfillment and to experience life through the five senses.',
     objImage:'img/3.jpg'
 
 },
 {
-    name:"yellow",
-    means:'as your favorite means you have a deep need for logical order in your everyday life and to be able to express your individuality by using your logical mind to inspire and create new ideas',
+    name:"Yellow",
+    means:'As your favorite means you have a deep need for logical order in your everyday life and to be able to express your individuality by using your logical mind to inspire and create new ideas',
     objImage:'img/4.jpg'
 
 },
 {
-    name:"blue",
-    means:'you have a deep need to find inner peace and truth, to live their life according to their ideals and beliefs without having to change their inflexible viewpoint of life to satisfy others.',
+    name:"Blue",
+    means:'You have a deep need to find inner peace and truth, to live their life according to their ideals and beliefs without having to change their inflexible viewpoint of life to satisfy others.',
     objImage:'img/5.jpg'
 
 },
 {
-    name:"pink",
-    means:'you have a deep need to be accepted and loved unconditionally',
+    name:"Pink",
+    means:'You have a deep need to be accepted and loved unconditionally',
     objImage:'img/6.jpg'
 
     
